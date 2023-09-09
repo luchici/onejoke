b2920e56d3e243fd39cdc1463432b49111e98fa5
luchici <luchici123@gmail.com>
2023-09-09 12:48:14 +0300

    initial commit

---
+++ src/test/java/com/github/luchici/oktaimpl/OktaimplApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.luchici.oktaimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaimplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
