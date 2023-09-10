9644e911e89799d0d2d59048260df9e52e958c28
luchici <luchici123@gmail.com>
2023-09-10 10:08:53 +0300

    update branch

--- src/main/java/com/github/luchici/oktaimpl/OktaimplApplication.java
+++ src/main/java/com/github/luchici/oktaimpl/OktaimplApplication.java
@@ -2,8 +2,10 @@ package com.github.luchici.oktaimpl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OktaimplApplication {
 
     public static void main(String[] args) {
