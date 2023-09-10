d9c27fceae72e8e3155d5b848fcf382681f0fe20
luchici <luchici123@gmail.com>
2023-09-10 15:14:35 +0300

    improved contextLoads() unit test
    Improve contextLoads() by:
    - testing if the context is correctly initialized
    - adding a more descriptive assertion

--- src/test/java/com/github/luchici/oktaimpl/OktaimplApplicationTests.java
+++ src/test/java/com/github/luchici/oktaimpl/OktaimplApplicationTests.java
@@ -1,12 +1,17 @@
 package com.github.luchici.oktaimpl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 class OktaimplApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(WebApplicationContext context) {
+        assertThat(context)
+            .isNotNull();
     }
 }
