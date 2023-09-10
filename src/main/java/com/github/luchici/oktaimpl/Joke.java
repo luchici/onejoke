9e709c63a073be25ad27583b5321c9ac704b6447
luchici <luchici123@gmail.com>
2023-09-10 15:48:11 +0300

    add Joke as a wrapper for the joke

---
+++ src/main/java/com/github/luchici/oktaimpl/Joke.java
@@ -0,0 +1,16 @@
+package com.github.luchici.oktaimpl;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Joke {
+
+    @JsonProperty("joke")
+    private String jokeText;
+
+}
