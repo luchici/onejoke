b3365e1efe73ea3e83efe0f892848901a978736d
luchici <luchici123@gmail.com>
2023-09-10 08:24:09 +0300

    pass joke and username strings to home view

--- src/main/java/com/github/luchici/oktaimpl/controllers/JokeController.java
+++ src/main/java/com/github/luchici/oktaimpl/controllers/JokeController.java
@@ -2,16 +2,21 @@ package com.github.luchici.oktaimpl.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/jokes")
+@RequestMapping
 public class JokeController {
 
-    @GetMapping("/just-one")
-    public String getJoke(){
-        return "joke";
+    @GetMapping
+    public String getHome(Model model) {
+        String username = "Brad Pitt";
+        String joke = "55gJJnc986f3s4VY08BHDVjS";
+        model.addAttribute("username", username);
+        model.addAttribute("joke", joke);
+        return "home";
     }
 }
