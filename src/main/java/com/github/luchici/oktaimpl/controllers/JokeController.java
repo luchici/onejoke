5a80add21d23d70da2682ff70a053cb56564b023
luchici <luchici123@gmail.com>
2023-09-10 15:28:54 +0300

    update JokeController to use JokeService

--- src/main/java/com/github/luchici/oktaimpl/controllers/JokeController.java
+++ src/main/java/com/github/luchici/oktaimpl/controllers/JokeController.java
@@ -1,5 +1,6 @@
 package com.github.luchici.oktaimpl.controllers;
 
+import com.github.luchici.oktaimpl.services.JokeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class JokeController {
 
+    private final JokeService jokeService;
+
     @GetMapping
     public String getHome(Model model) {
         String username = "Brad Pitt";
-        String joke = "55gJJnc986f3s4VY08BHDVjS";
+        String joke = jokeService.getJoke();
         model.addAttribute("username", username);
         model.addAttribute("joke", joke);
         return "home";
