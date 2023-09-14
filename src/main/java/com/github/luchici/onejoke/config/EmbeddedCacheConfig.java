743e61dc9a6574e17b3f7910914a15cc3b6aeb5f
luchici <luchici123@gmail.com>
2023-09-14 12:21:05 +0300

    enable spring cache

---
+++ src/main/java/com/github/luchici/onejoke/config/EmbeddedCacheConfig.java
@@ -0,0 +1,11 @@
+package com.github.luchici.onejoke.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class EmbeddedCacheConfig {
+
+
+}
