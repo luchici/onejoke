3da6e397a74222cdabb4252f9e741db7a9cc9ec2
luchici <luchici123@gmail.com>
2023-10-07 12:57:26 +0300

    add SVG template resolver

---
+++ src/main/java/com/github/luchici/onejoke/ThymeleafConfig.java
@@ -0,0 +1,19 @@
+package com.github.luchici.onejoke;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public ITemplateResolver svgTemplate(){
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/svg/");
+        resolver.setSuffix(".svg");
+        resolver.setTemplateMode("XML");
+        return resolver;
+    }
+}
