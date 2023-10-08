e5789899ef1f7696247a105a75f07bf91eee4f3a
luchici <luchici123@gmail.com>
2023-10-08 18:21:20 +0300

    update JokeController slice tests by updating home page testcase

--- src/test/java/com/github/luchici/onejoke/controllers/JokeControllerTest.java
+++ src/test/java/com/github/luchici/onejoke/controllers/JokeControllerTest.java
@@ -1,16 +1,24 @@
 package com.github.luchici.onejoke.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import com.github.luchici.onejoke.ThymeleafConfig;
 import com.github.luchici.onejoke.services.JokeService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(JokeController.class)
+@Import({ThymeleafConfig.class})
 public class JokeControllerTest {
 
     @Autowired
@@ -18,14 +26,17 @@ public class JokeControllerTest {
     @MockBean
     private JokeService jokeService;
 
-    @Test
-    void getHome_ReturnsHomeView() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = {"/", "/home"})
+    void getHome_ReturnsHomeView(String path) throws Exception {
         // Given
-        String expectedViewName = "home";
+
         // When
-        mockMvc.perform(get("/"))
-        // Then
-                    .andExpect(
-                        view().name(expectedViewName));
+        mockMvc.perform(get(path))
+            // Then
+            .andExpectAll(
+                status().isOk(),
+                view().name("home"),
+                content().contentType(MediaType.TEXT_HTML_VALUE+";charset=UTF-8"));
     }
 }
