5c491c79f0e2417025efef5468c8da6ef1f6d54f
luchici <luchici123@gmail.com>
2023-10-10 21:48:22 +0300

    Move config classes and add object mapper bean to serialize LocalDate
    Moved config classes to the config folder.
    Added a new class GeneralConfig that declares an object mapper bean
    with the JavaTimeModule registered. This is necessary to be able to
    serialize LocalDate.

---
+++ src/main/java/com/github/luchici/onejoke/config/GeneralConfig.java
@@ -0,0 +1,22 @@
+package com.github.luchici.onejoke.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class GeneralConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper javaTimeModule() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return mapper;
+    }
+
+}
--- src/main/java/com/github/luchici/onejoke/config/ThymeleafConfig.java
+++ src/main/java/com/github/luchici/onejoke/config/ThymeleafConfig.java
@@ -1,4 +1,4 @@
-package com.github.luchici.onejoke;
+package com.github.luchici.onejoke.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
