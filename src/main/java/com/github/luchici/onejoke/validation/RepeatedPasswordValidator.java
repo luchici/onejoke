05ce0727cd2843d3c78c74ca141d0d7da59058a5
luchici <luchici123@gmail.com>
2023-10-19 11:08:48 +0300

    add class validation annotation RepeatedPasswordValidator

--- src/main/java/com/github/luchici/onejoke/validation/RepeatedPasswordValidator.java
+++ src/main/java/com/github/luchici/onejoke/validation/RepeatedPasswordValidator.java
@@ -1,14 +1,14 @@
 package com.github.luchici.onejoke.validation;
 
-import com.github.luchici.onejoke.model.UserUpdate;
+import com.github.luchici.onejoke.model.UserUpdateFormData;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Objects;
 
-public class RepeatedPasswordValidator implements ConstraintValidator<RepeatedPassword, UserUpdate> {
+public class RepeatedPasswordValidator implements ConstraintValidator<RepeatedPassword, UserUpdateFormData> {
 
     @Override
-    public boolean isValid(UserUpdate value, ConstraintValidatorContext context) {
+    public boolean isValid(UserUpdateFormData value, ConstraintValidatorContext context) {
         return Objects.equals(value.getPassword(), value.getRepeatedPassword());
     }
 }
